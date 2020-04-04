93d8ef03e13fae8fddceb4388ff99fdccbdb8679
Jotaro <jojojoe77@gmail.com>
2020-04-05 01:57:04 +0200

    playing with react

--- src/App.js
+++ src/App.js
@@ -1,49 +1,19 @@
 import React, {Component} from 'react';
-import Button from 'react-bootstrap/Button';
 import Navigation from './components/ui/Navigation';
+import Main from './components/visualization/Main';
+
 class App extends Component{
   render(){
     return(
-      <div>  {/* TODO: move contaier class here from Navigation.*/}
+      <div className="container-fluid">
+      <div className="row">
         {/* <Header /> */}
         <Navigation />
-        <Counter />
+        <Main />
         {/* <Footer /> */}
       </div>
-    );
-  }
-}
-
-class Counter extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      value:0
-    };
-  }
-
-  onIncrement = ()=>{
-    this.setState({value : Math.min(10, this.state.value + 1)});
-  }
-
-  onDecrement = ()=>{
-    this.setState({value : Math.max(0, this.state.value - 1)});
-  }
-
-  render(){
-    return(
-      <div>
-        <div>
-          counts : {this.state.value}
-        </div>
-        <div>
-        <button type="button" onClick={this.onIncrement}>+</button>
-        <button type="button" onClick={this.onDecrement}>-</button>
-        <Button onClick={this.onDecrement}>うんこ</Button>
-        </div>
-
       </div>
-    )
+    );
   }
 }
 
